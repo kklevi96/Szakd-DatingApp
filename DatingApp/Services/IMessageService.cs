0e238d09b0af40b206ae3a49ee03d6398db04039
kklevi96 <kklevi@gmail.com>
2026-04-07 22:38:42 +0200

    meeting init, message ready

---
+++ DatingApp/Services/IMessageService.cs
@@ -0,0 +1,10 @@
+﻿using DatingApp.Dtos.Messages;
+
+namespace DatingApp.Services
+{
+    public interface IMessageService
+    {
+        Task<MessageResponse> SendMessageAsync(string currentUserId, SendMessageRequest request);
+        Task<List<MessageResponse>> GetMessagesByConversationAsync(string currentUserId, int conversationId);
+    }
+}
